69b82032351e2a080770793891bc2b10c832d271
Johannes-Pandiangan <johannesindra@students.usu.ac.id>
2025-11-25 19:05:23 +0700

    Update API URL in Home untuk events

--- event-kampus/src/pages/Home.jsx
+++ event-kampus/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import SeminarIlkom from "../assets/SeminarIlkom.png";
 import Pelatihan from "../assets/Pelatihan.png"; 
 
 // Base URL untuk API backend
-const BASE_URL = '/api/events';
+const BASE_URL = 'https://event-kampus-johannes.vercel.app/api/events';
 
 // Mapping aset lokal untuk event awal yang diinisialisasi
 const IMAGE_MAP = {
@@ -222,3 +222,4 @@ function Home() {
 
 
 export default Home;
+
